8baa1c0e2f587c1da91c7c834d7b198c56dd671d
Satyam Pandey <131981685+DevXPanda@users.noreply.github.com>
2026-03-02 13:02:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "ulbsystemm.vercel.app",
+  "https://ulbsystemm.vercel.app",
 ];
 
 app.use(
